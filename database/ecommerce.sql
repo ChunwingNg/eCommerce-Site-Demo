c754dc3098a159723fe164eee54b4a5fdbe0c01d
Chunwing Ng <chunwing1030760@gmail.com>
2020-10-28 18:12:31 -0400

    Make .SQL file compatible with xampp

--- database/ecommerce.sql
+++ database/ecommerce.sql
@@ -20,36 +20,14 @@ USE `ecommerce` ;
 DROP TABLE IF EXISTS `ecommerce`.`user` ;
 
 CREATE TABLE IF NOT EXISTS `ecommerce`.`user` (
-  `id` INT NOT NULL,
-  `userN` VARCHAR(45) NULL,
-  `passW` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `userN` VARCHAR(45) NOT NULL,
+  `passW` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `ecommerce`.`card`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `ecommerce`.`card` ;
-
-CREATE TABLE IF NOT EXISTS `ecommerce`.`card` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `exp` DATE NULL,
-  `number` INT NULL,
-  `cvv` INT NULL,
-  `fk_card_user` INT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_card_user_idx` (`fk_card_user` ASC) VISIBLE,
-  UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE,
-  CONSTRAINT `fk_card_user`
-    FOREIGN KEY (`fk_card_user`)
-    REFERENCES `ecommerce`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  UNIQUE INDEX `ID_UNIQUE` (`id` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -58,38 +36,49 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `ecommerce`.`address` ;
 
 CREATE TABLE IF NOT EXISTS `ecommerce`.`address` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `line1` VARCHAR(45) NULL,
-  `apt` VARCHAR(45) NULL,
-  `line2` VARCHAR(45) NULL,
-  `phone` INT NULL,
-  `fk_address_user` INT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `line1` VARCHAR(45) NOT NULL,
+  `apt` VARCHAR(45) NULL DEFAULT NULL,
+  `city` VARCHAR(45) NOT NULL,
+  `state` VARCHAR(45) NOT NULL,
+  `zip` INT(11) NOT NULL,
+  `phone` INT(11) NOT NULL,
+  `fk_address_user` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_address_user_idx` (`fk_address_user` ASC) VISIBLE,
-  UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `ID_UNIQUE` (`id` ASC),
+  INDEX `fk_address_user_idx` (`fk_address_user` ASC),
   CONSTRAINT `fk_address_user`
     FOREIGN KEY (`fk_address_user`)
     REFERENCES `ecommerce`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `ecommerce`.`catalog_item`
+-- Table `ecommerce`.`card`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ecommerce`.`catalog_item` ;
+DROP TABLE IF EXISTS `ecommerce`.`card` ;
 
-CREATE TABLE IF NOT EXISTS `ecommerce`.`catalog_item` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `price` FLOAT NULL,
-  `quantity` INT NULL,
-  `desc` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `ecommerce`.`card` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `exp` DATE NOT NULL,
+  `number` INT(11) NOT NULL,
+  `cvv` INT(11) NOT NULL,
+  `fk_card_user` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB;
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `fk_card_user_idx` (`fk_card_user` ASC),
+  CONSTRAINT `fk_card_user`
+    FOREIGN KEY (`fk_card_user`)
+    REFERENCES `ecommerce`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -98,11 +87,29 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `ecommerce`.`catagory_name` ;
 
 CREATE TABLE IF NOT EXISTS `ecommerce`.`catagory_name` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB;
+  UNIQUE INDEX `ID_UNIQUE` (`id` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `ecommerce`.`catalog_item`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `ecommerce`.`catalog_item` ;
+
+CREATE TABLE IF NOT EXISTS `ecommerce`.`catalog_item` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `price` FLOAT NOT NULL,
+  `quantity` INT(11) NOT NULL,
+  `desc` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `ID_UNIQUE` (`id` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -111,10 +118,10 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `ecommerce`.`categories` ;
 
 CREATE TABLE IF NOT EXISTS `ecommerce`.`categories` (
-  `fk_cat_catalog` INT NOT NULL,
-  `fk_cat_catname` INT NOT NULL,
-  INDEX `fk_cat_catalog_idx` (`fk_cat_catalog` ASC) VISIBLE,
-  INDEX `fk_cat_catname_idx` (`fk_cat_catname` ASC) VISIBLE,
+  `fk_cat_catalog` INT(11) NOT NULL,
+  `fk_cat_catname` INT(11) NOT NULL,
+  INDEX `fk_cat_catalog_idx` (`fk_cat_catalog` ASC),
+  INDEX `fk_cat_catname_idx` (`fk_cat_catname` ASC),
   CONSTRAINT `fk_cat_catalog`
     FOREIGN KEY (`fk_cat_catalog`)
     REFERENCES `ecommerce`.`catalog_item` (`id`)
@@ -125,7 +132,8 @@ CREATE TABLE IF NOT EXISTS `ecommerce`.`categories` (
     REFERENCES `ecommerce`.`catagory_name` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
