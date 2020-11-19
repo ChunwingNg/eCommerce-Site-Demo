ded152609f249636b615f7fa2d2d847e1f3b4a8d
Zoey <zoey9218@gmail.com>
2020-11-18 22:39:47 -0500

    Revert "nav_bar css & others"
    This reverts commit 31a92d1e3f74feb8e0cab904680cb7cb75196c6b.

--- php/add.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include ('database.php');
-    $userName = $_POST['user_name'];
-    echo $userName;
-    $query = "INSERT INTO `user` ('userN')
-            VALUES ('$userName')";
-    $db -> exec($query);
-    include ('checkOut.php');
-?>
