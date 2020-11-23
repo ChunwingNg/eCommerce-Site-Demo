d0213df2c27b00dc43625ebca10359961951e14a
Zoey <zoey9218@gmail.com>
2020-11-23 11:37:48 -0500

    userpage css

--- php/userpage.php
+++ php/userpage.php
@@ -15,86 +15,91 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Info</title>
-    <link rel="stylesheet" type="text/css" href="../css/userpage.css">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../css/userPage.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
-
-<body>
-<h1>Account Overview</h1>
-
-<div>
-    <h3>Contact Information</h3>
-    <p>Username: <?php echo $_SESSION['name']; ?></p>
-    <p>Email: <?php echo $_SESSION['email']; ?></p>
-    <br>
-    <a href="">Edit</a>
-    <br>
-    <a href="">Change Password</a>
-</div>
-
-<div>
-    <form action="addr-add.php" method="post" id="userpage">
-        <!-- Address form -->
-            <h2>Address</h2>
-            <!-- address input-->
-            <label class="control-label">Street Address</label>
-            <div class="controls">
-                <input id="address-line" name="address-line" type="text" placeholder="Address line"
-                class="input-xlarge">
-                <p class="help-block"></p>
-            </div>
-            <!-- city input-->
-            <label class="control-label">City</label>
-            <div class="controls">
-                <input id="city" name="city" type="text" placeholder="City" class="input-xlarge">
-                <p class="help-block"></p>
-            </div>
-            <!-- state input-->
-            <label class="control-label">State</label>
-            <div class="controls">
-                    <input id="state" name="state" type="text" placeholder="State"
-                    class="input-xlarge">
-                    <p class="help-block"></p>
-            </div>
-            <!-- postal-code input-->
-                <label class="control-label">Zip / Postal Code</label>
-                <div class="controls">
-                    <input id="postal-code" name="postal-code" type="text" placeholder="Zip"
-                    class="input-xlarge">
-                    <p class="help-block"></p>
-            </div>
-            <input type="submit" name="addr-add" value="Add New Address"></button>
-        <br>
-    </form>
-
-    <h3>Address Book</h3>
     
-    <?php foreach ($addresses as $address):?>
-    <div>
-        <p><?php echo $address['name'];?></p>
-        <p><?php echo $address['line1'];?></p>
-        <p><?php echo $address['line2'];?></p>
-        <p><?php echo $address['city'];?>, <?php echo $address['state'];?> <?php echo $address['zip'];?></p>
-        <form action="addr-delete.php" method="post">
-            <input type="hidden" name="addr_id" value=<?php echo $address['id'];?>>
-            <input type="submit" value="Delete Address">
-        </form>
+<body>
+    <h1>Account Overview</h1>
+
+<!-- contact info -->
+    <div id="contact_info">
+        <h2>Contact Information</h2>
+        <br>
+        <p>Username: <?php echo $_SESSION['name']; ?></p>
+        <p>Email: <?php echo $_SESSION['email']; ?></p>
+        <br>
+        <button type="button" ><a href="">Edit</a></button>
+        <br>
+        <button type="button"><a href="">Change Password</a></button>
     </div>
-    <?php endforeach;?>
-</div>
 
-<div>
-    <h3>Payment Information</h3>
-
-    <?php foreach ($cards as $card):?>
-        <p>**** **** **** <?php echo substr ($card['number'], -4);?></p>
-        <form action="del_card.php" method="post">
-            <input type="hidden" name="card_id" value=<?php echo $card['id'];?>>
-            <input type="submit" value="Delete Card">
+    <div id="address_form">
+        <form action="addr-add.php" method="post" id="userpage">
+            <!-- Address form -->
+                <h2>Address</h2>
+                <br>
+                <!-- address input-->
+                <label class="control-label">Street Address</label>
+                <div class="controls">
+                    <input id="address-line" name="address-line" type="text" placeholder="Address line"
+                    class="input-xlarge">
+                    <p class="help-block"></p>
+                </div>
+                <!-- city input-->
+                <label class="control-label">City</label>
+                <div class="controls">
+                    <input id="city" name="city" type="text" placeholder="City" class="input-xlarge">
+                    <p class="help-block"></p>
+                </div>
+                <!-- state input-->
+                <label class="control-label">State</label>
+                <div class="controls">
+                        <input id="state" name="state" type="text" placeholder="State"
+                        class="input-xlarge">
+                        <p class="help-block"></p>
+                </div>
+                <!-- postal-code input-->
+                    <label class="control-label">Zip / Postal Code</label>
+                    <div class="controls">
+                        <input id="postal-code" name="postal-code" type="text" placeholder="Zip"
+                        class="input-xlarge">
+                        <p class="help-block"></p>
+                </div>
+                <input type="submit" name="addr-add" value="Add New Address"></button>
+            <br>
         </form>
-    <?php endforeach;?>
 
-    <a href="">Add New Card</a>
-</div>
-    </body>
+        <h3>Address Book</h3>
+        
+        <?php foreach ($addresses as $address):?>
+        <div>
+            <p><?php echo $address['name'];?></p>
+            <p><?php echo $address['line1'];?></p>
+            <p><?php echo $address['line2'];?></p>
+            <p><?php echo $address['city'];?>, <?php echo $address['state'];?> <?php echo $address['zip'];?></p>
+            <form action="addr-delete.php" method="post">
+                <input type="hidden" name="addr_id" value=<?php echo $address['id'];?>>
+                <input type="submit" value="Delete Address">
+            </form>
+        </div>
+        <?php endforeach;?>
+    <hr>
+
+    <div id="payment_info">
+        <h3>Payment Information</h3>
+
+        <?php foreach ($cards as $card):?>
+            <p>**** **** **** <?php echo substr ($card['number'], -4);?></p>
+            <form action="del_card.php" method="post">
+                <input type="hidden" name="card_id" value=<?php echo $card['id'];?>>
+                <input type="submit" value="Delete Card">
+            </form>
+        <?php endforeach;?>
+
+        <button type="button"><a href="">Add New Card</a></button>
+    </div>
+    </div>
+</body>
 </html>
