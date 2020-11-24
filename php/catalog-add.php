78e0190dc138ac63a7c1f5dfa6e54b822e42bf39
Kyle <kra52195@uga.edu>
2020-11-23 22:09:50 -0500

    Added in all catalog item pics

--- php/catalog-add.php
+++ php/catalog-add.php
@@ -9,24 +9,10 @@
     $desc = $_POST['desc'];
     $link = $_POST['img_link'];
 
-    if ($street == null || $unit == null || $city == null || $state == null || $zip == null || $userId == null) 
-    {
-        $error = "Invalid address data. Try again.";
-
-        ?>
-        <script>
-           alert("Error: Please make sure to fill in address fields correctly");
-        </script>
-        <?php
-        $_SESSION['error'] = 1;
-        header("Location: address-form.php");
-    } 
-    else
-    {
 
     //Queries the row the username is on
-    $db->exec("INSERT INTO catalog_item(`name`,price,quantity,`desc`,image_link) VALUES($name, $price $quantity, $desc, $link)";
+    $db->exec("INSERT INTO catalog_item(`name`,price,quantity,`desc`,image_link) VALUES($name, $price $quantity, $desc, $link)");
 
-    header("Location: catalog-form.php");
-    }
+   
+    
 ?>
